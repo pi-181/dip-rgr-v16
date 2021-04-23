f7bac0cfe64f054afbfdbc0e9f4f6e335fff564a
demkom58 <demkom58@gmail.com>
2021-04-23 23:31:08 +0300

    Initial commit

---
+++ gaussian.m
@@ -0,0 +1,44 @@
+% Gaussian blur algorithm
+figure('NumberTitle', 'off', 'Name', 'Image');
+
+uicontrol('String', 'Blur strength:', 'Units', 'normalized', 'Style', 'text', 'Position', [0 0.024 0.18 0.07]);
+uicontrol('String', "Image name:", 'Units', 'normalized', 'Style', 'text', 'Position', [0.17 0.024 0.18 0.07]);
+
+uicontrol('String', "10", 'Style', 'edit', 'Units', 'normalized', 'Position', [0.04 0 0.1 0.06], 'Tag', 'strengthField');
+uicontrol('String', "photo.jpg", 'Style', 'edit', 'Units', 'normalized', 'Position', [0.18 0 0.16 0.06], 'Tag', 'imageField');
+
+uicontrol('String', 'Execute', 'Callback', @process, 'Units', 'normalized', 'Position', [0.36 0 0.15 0.06]);
+
+function process(~, ~)
+    strengthField = findobj(gcf, 'Tag', 'strengthField');
+    imageField = findobj(gcf, 'Tag', 'imageField');
+
+    strengthField = strengthField.String;
+    imageField = imageField.String;
+
+    if isempty(strengthField) || isempty(imageField)
+        fprintf('Error: fill all fields!\n')
+        return;
+    end
+
+    strength = floor(str2num(['double(', strengthField, ')']));
+
+    rgbImage = imread(imageField); % Read image from file
+    subplot(1, 2, 1), imshow(rgbImage); % Display original image
+    title('Оригінальне зображення');
+
+    % Generate coordinates with the origin in  middle
+    ind = -floor(strength / 2) : floor(strength / 2);
+    [X, Y] = meshgrid(ind, ind);
+
+    % Create Gaussian Mask
+    sigma = std2(rgbImage);
+    mask = exp(-(X .^ 2 + Y .^ 2) / (2 * sigma ^ 2));
+
+    % Normalize
+    mask = mask / sum(mask(:));
+
+    blurredImage = imfilter(rgbImage, mask, 'replicate'); % Blur the image.
+    subplot(1, 2, 2), imshow(blurredImage); % Display blured image
+    title('Розмите зображення (Gaussian)');
+end
---
+++ meadian.m
@@ -0,0 +1,38 @@
+% Median blur algorithm
+figure('NumberTitle', 'off', 'Name', 'Image');
+
+uicontrol('String', 'Blur strength:', 'Units', 'normalized', 'Style', 'text', 'Position', [0 0.024 0.18 0.07]);
+uicontrol('String', "Image name:", 'Units', 'normalized', 'Style', 'text', 'Position', [0.17 0.024 0.18 0.07]);
+
+uicontrol('String', "10", 'Style', 'edit', 'Units', 'normalized', 'Position', [0.04 0 0.1 0.06], 'Tag', 'strengthField');
+uicontrol('String', "photo.jpg", 'Style', 'edit', 'Units', 'normalized', 'Position', [0.18 0 0.16 0.06], 'Tag', 'imageField');
+
+uicontrol('String', 'Execute', 'Callback', @process, 'Units', 'normalized', 'Position', [0.36 0 0.15 0.06]);
+
+function process(~, ~)
+    strengthField = findobj(gcf, 'Tag', 'strengthField');
+    imageField = findobj(gcf, 'Tag', 'imageField');
+
+    strengthField = strengthField.String;
+    imageField = imageField.String;
+
+    if isempty(strengthField) || isempty(imageField)
+        fprintf('Error: fill all fields!\n')
+        return;
+    end
+
+    strength = floor(str2num(['double(', strengthField, ')']));
+
+    rgbImage = imread(imageField); % Read image from file
+    subplot(1, 2, 1), imshow(rgbImage); % Display original image
+    title('Оригінальне зображення');
+
+    blurredImage = rgbImage;
+    % apply median filter for each channel
+    for c = 1 : 3
+        blurredImage(:, :, c) = medfilt2(rgbImage(:, :, c), [strength, strength]);
+    end
+    
+    subplot(1, 2, 2), imshow(blurredImage); % Display blured image
+    title('Розмите зображення (Median)');
+end
